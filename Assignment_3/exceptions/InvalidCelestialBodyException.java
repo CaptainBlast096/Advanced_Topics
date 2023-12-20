a5bd77842ba2863bf2e40f0b89b68daa38b6171a
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2023-12-20 09:54:08 -0500

    Add files via upload

---
+++ Assignment_3/exceptions/InvalidCelestialBodyException.java
@@ -0,0 +1,15 @@
+package exceptions;
+/**
+ * @author Jaleel A. Rogers
+ * Student Number: 11239
+ * Date: 3/26/2023
+ * Major: Computer Science
+ *Exception is responsible for improper Star, Planet, and Moon objects being attached to each other
+ */
+public class InvalidCelestialBodyException extends Exception
+{
+    public InvalidCelestialBodyException (String argMessage)
+    {
+        super (argMessage);
+    }
+}
